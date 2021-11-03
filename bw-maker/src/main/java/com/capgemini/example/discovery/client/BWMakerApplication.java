3228468627b4a022614da99de78a97f866425cbf
Robert Malinkiewicz <robert.malinkiewicz@capgemini.com>
2021-11-03 14:48:54 +0100

    eureka-client bw-maker - initial commit

---
+++ bw-maker/src/main/java/com/capgemini/example/discovery/client/BWMakerApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.example.discovery.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BWMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BWMakerApplication.class, args);
+	}
+}
---
+++ bw-maker/src/main/java/com/capgemini/example/discovery/client/controller/ServiceAppNameRestController.java
@@ -0,0 +1,24 @@
+package com.capgemini.example.discovery.client.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class ServiceAppNameRestController {
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping("/name")
+    public String serviceApplicationName() {
+        return "Hi, I'm " + appName + " on " + serverPort;
+    }
+}
