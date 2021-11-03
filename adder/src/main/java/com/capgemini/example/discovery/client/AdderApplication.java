e51110d6f6b8d5c18a922a1881ba7263369e3bfd
Robert Malinkiewicz <robert.malinkiewicz@capgemini.com>
2021-11-03 14:48:09 +0100

    eureka-client adder - initial commit

---
+++ adder/src/main/java/com/capgemini/example/discovery/client/AdderApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.example.discovery.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdderApplication.class, args);
+	}
+}
+
